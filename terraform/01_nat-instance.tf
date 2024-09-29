2006b3c6b2b65df7db209effb6f31a9b4d13ced8
SeSloup <gekasologub@gmail.com>
2024-09-29 17:25:08 +0300

    README final

--- terraform/01_nat-instance.tf
+++ terraform/01_nat-instance.tf
@@ -139,13 +139,6 @@ resource "yandex_vpc_security_group" "nat-instance-sg2" {
     protocol       = "TCP"
     description    = "ext-http-zabix"
     v4_cidr_blocks = ["0.0.0.0/0"]
-    port           = 10050
-  }
-
-  ingress {
-    protocol       = "TCP"
-    description    = "ext-http-zabix"
-    v4_cidr_blocks = ["0.0.0.0/0"]
     port           = 10051
   }
 /*
